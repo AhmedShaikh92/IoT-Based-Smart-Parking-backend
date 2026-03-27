3d05e0453677e297094e396b02882b79d6d24570
AhmedShaikh92 <ahmedshaikhameenmiyan12@gmail.com>
2026-03-27 15:50:45 +0530

    cors

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://iot-based-smart-parking.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 
 //ping to prevent idling on Render
